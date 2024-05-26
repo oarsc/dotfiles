6f2ba21053bc664aa3f3b7c5ee1ae536b566132f
scopp <osc.car2@gmail.com>
2024-05-26 20:20:02 +0200

    Move all nodes when switching off/on monitors

--- bspwm/scripts/enable_screen.sh
+++ bspwm/scripts/enable_screen.sh
@@ -11,36 +11,58 @@ function array() {
     IFS=' ' read -r -a output <<< "$value"
 }
 
-SELECTED_MONITOR=$1
+INDEX=$(($1-1))
 
 array MONITORS "$(xrandr --query | grep " connected" | cut -d" " -f1)"
-LENGTH=${#MONITORS[@]}
+array RESOLUTIONS "$(xrandr --query | grep " connected" -A 1 | grep -v " connected" | grep -v '\-\-' | awk '{print $1}')"
+
 POSITIONS=( "0x0 --rate 144" 2560x180 )
 
-if [ $LENGTH -lt $SELECTED_MONITOR ]; then
+LENGTH=${#MONITORS[@]}
+
+if [ $LENGTH -lt $((INDEX + 1)) ]; then
     >&2 echo "Monitor index out of range ($LENGTH)";
     exit 1
 fi
 
 if type "xrandr"; then
-    if [ $SELECTED_MONITOR -eq 0 ]; then
-
-        array RESOLUTIONS "$(xrandr --query | grep " connected" -A 1 | grep -v " connected" | grep -v '\-\-' | awk '{print $1}')"
+    if [ $INDEX -eq -1 ]; then
 
         for (( i = 0 ; i < ${#MONITORS[@]} ; i++ )); do
             xrandr --output ${MONITORS[$i]} --mode ${RESOLUTIONS[$i]} --pos ${POSITIONS[$i]} --rotate normal
         done
 
     else
-        for i in $(seq 1 $LENGTH); do
-            if [ $i -ne $SELECTED_MONITOR ]; then
-                bspc monitor ${MONITORS[$i-1]} -r
-                xrandr --output ${MONITORS[$i-1]} --off
+
+        # We need both monitors and bspwm on them to move the elements to the current monitor
+        for (( i = 0 ; i < ${#MONITORS[@]} ; i++ )); do
+            xrandr --output ${MONITORS[$i]} --mode ${RESOLUTIONS[$i]} --pos ${POSITIONS[$i]} --rotate normal
+        done
+
+        ~/.config/bspwm/bspwmrc
+
+        monitorDesktops=($(bspc wm -d | jq -r '.monitors[].desktops | map(.id) | tostring | "" + . + ""'))
+        selectedMonitorDesktops=($(echo ${monitorDesktops[$INDEX]} | jq '.[]'))
+        
+        for i in $(seq 0 $(($LENGTH - 1))); do
+            if [ $i -ne $INDEX ]; then
+
+                desktops=($(echo ${monitorDesktops[$i]} | jq '.[]'))
+
+                for j in $(seq 0 $((${#selectedMonitorDesktops[@]} - 1))); do
+                    nodes=$(bspc query -N -d ${desktops[$j]})
+                    for node in $nodes; do
+                        bspc node $node -d ${selectedMonitorDesktops[$j]}
+                    done
+                done
+
+                bspc monitor ${MONITORS[$i]} -r
+                xrandr --output ${MONITORS[$i]} --off
             fi
         done
 
-        RESOLUTION=$(xrandr --query | grep " connected" -A 1 | grep -v " connected" | grep -v '\-\-' | awk '{print $1}' | sed -n "$SELECTED_MONITOR p")
-        MONITOR=${MONITORS[$SELECTED_MONITOR-1]}
+        RESOLUTION=$(xrandr --query | grep " connected" -A 1 | grep -v " connected" | grep -v '\-\-' | awk '{print $1}' | sed -n "$1 p")
+        MONITOR=${MONITORS[$INDEX]}
 
         xrandr --output $MONITOR --mode $RESOLUTION --pos 0x0 --rotate normal
     fi
