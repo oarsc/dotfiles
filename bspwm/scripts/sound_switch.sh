ca38a3851b392141e665e628960f07709966b9f6
scopp <osc.car2@gmail.com>
2024-09-11 19:16:06 +0200

    Update sound script

--- bspwm/scripts/sound_switch.sh
+++ bspwm/scripts/sound_switch.sh
@@ -5,21 +5,25 @@ CURRENT=$(pactl info | grep "Default Sink: " | sed "s/Default Sink\: //")
 found=false
 for sink in $(pactl list short sinks); do
     if  [[ $sink == alsa_output* ]]; then
-        if [ -z "$first_sink" ]; then
-            first_sink=$sink
+        if [ -z "$sink_to_use" ]; then
+            sink_to_use=$sink
         fi
 
         if  [[ $sink == $CURRENT ]]; then
             found=true
         else
-            if [ $found == true ]; then
-                pactl set-default-sink "$sink"
-                notify-send $(echo $sink | sed "s/alsa_output\.//")
-                exit 0
+            if [[ $found == true && $sink != *hdmi* ]]; then
+                sink_to_use="$sink"
+                break
             fi
         fi
     fi
 done
 
-pactl set-default-sink "$first_sink"
-notify-send $(echo $first_sink | sed "s/alsa_output\.//")
+pactl set-default-sink "$sink_to_use"
+for stream in $(pactl list short sink-inputs | cut -f1); do
+	pactl move-sink-input "$stream" "$sink_to_use"
+done
+
+sink_name="$(pactl list sinks | grep -A 20 "$sink_to_use" | grep "alsa.card_name" | cut -d '"' -f 2)"
+notify-send "Audio Device: $sink_name"
